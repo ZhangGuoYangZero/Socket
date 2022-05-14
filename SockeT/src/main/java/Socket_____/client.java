f91688decb58da82e2510f799c4fe5a0a0563515
张国杨Zero <101501786+ZhangGuoYangZero@users.noreply.github.com>
2022-05-14 17:27:27 +0800

    睡醒后解决了BUG

---
+++ SockeT/src/main/java/Socket_____/client.java
@@ -0,0 +1,41 @@
+package Socket_____;
+
+import org.junit.Test;
+
+import javax.imageio.stream.FileImageInputStream;
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class client {
+    @Test
+    public  void  client() throws IOException {
+        Socket socket  = new Socket(InetAddress.getLocalHost(),8888);
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(socket.getOutputStream());
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream("src/main/resources/t.jpg"));
+        Integer size = 0;
+        byte[] bytes = new byte[256];
+        while ( (size = bufferedInputStream.read(bytes) )!= -1){
+            bufferedOutputStream.write(bytes,0,size);
+        }
+        //flush在循环里面刷更快
+        bufferedOutputStream.flush();
+        socket.shutdownOutput();
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream(),"utf-8"));
+        String text = null;
+        while ((text = bufferedReader.readLine()) != null){
+            System.out.println(text);
+        }
+        socket.shutdownInput();
+
+
+        bufferedReader.close();
+        bufferedInputStream.close();
+        bufferedOutputStream.close();
+        socket.close();
+
+
+
+    }
+}
---
+++ SockeT/src/main/java/Socket_____/server.java
@@ -0,0 +1,47 @@
+package Socket_____;
+
+import org.junit.Test;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class server {
+
+    @Test
+    public void Server() throws IOException {
+        ServerSocket  serverSocket = new ServerSocket(8888);
+        Socket sockets = serverSocket.accept();
+
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(sockets.getInputStream());
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream( new FileOutputStream("src/main/resources/b.jpg"));
+        byte[] bytes = new byte[256];
+        Integer size = null;
+
+        while ((size = bufferedInputStream.read(bytes))!= -1){
+            bufferedOutputStream.write(bytes,0,size);
+            bufferedOutputStream.flush();
+            //flush适合在循环里面刷
+        }
+        sockets.shutdownInput();
+
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(sockets.getOutputStream(),"utf-8"));
+        bufferedWriter.write("已经收到!");
+        bufferedWriter.flush();
+        sockets.shutdownOutput();
+
+
+
+
+
+        bufferedWriter.close();
+        bufferedOutputStream.close();
+        bufferedInputStream.close();
+
+        sockets.close();
+        serverSocket.close();
+
+
+    }
+}
